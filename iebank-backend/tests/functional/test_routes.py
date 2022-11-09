2d01358149d6074cfb376061eb007ae74544b399
farahabuatiyeh <116284954+farahabuatiyeh@users.noreply.github.com>
2022-11-09 02:16:11 +0100

    Add files via upload

---
+++ iebank-backend/tests/functional/test_routes.py
@@ -0,0 +1,47 @@
+from iebank_api import app, db
+import pytest
+
+def test_get_accounts(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.get('/accounts')
+    assert response.status_code == 200
+
+def test_dummy_wrong_path():
+    """
+    GIVEN a Flask application
+    WHEN the '/wrong_path' page is requested (GET)
+    THEN check the response is valid
+    """
+    with app.test_client() as client:
+        response = client.get('/wrong_path')
+        assert response.status_code == 404
+
+def test_create_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is posted to (POST)
+    THEN check the response is valid
+    """
+    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
+    assert response.status_code == 200
+
+def test_get_account(testing_client):
+    response = testing_client.get('/accounts/1')
+    assert response.status_code == 200
+
+def test_delete_account(testing_client):
+    response = testing_client.delete('/accounts/1')
+    assert response.status_code == 200
+
+def test_update_account(testing_client):
+    response = testing_client.put('/accounts/1', json = {'name': 'John Doe', 'currency': '€'})
+    assert response.status_code == 200
+
+
+
+
+
